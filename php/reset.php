956d25c958deac5330c3300a37d3a5f1a8108448
Samson Olamide <olaorosamson3@gmail.com>
2022-06-13 12:59:28 +0100

    Reset password implemented

--- php/reset.php
+++ php/reset.php
@@ -1,15 +1,52 @@
 <?php
 if(isset($_POST['submit'])){
-    $email = //complete this;
-    $newpassword = //complete this;
 
-    resetPassword($email, $password);
+
+    $email =  $_POST['email'];
+    $newpassword = $_POST['password'];
+
+    resetPassword($email, $newpassword);
 }
 
-function resetPassword($email, $password){
-    //open file and check if the username exist inside
-    //if it does, replace the password
+function resetPassword($email, $newpassword){
+      
+  if (($open = fopen("../storage/users.csv", "r")) !== FALSE) 
+  {
+  
+    if (($data = fgetcsv($open, 1000, ",")) !== FALSE) 
+    {    
+        
+        $username = $data[0];
+        $email_saved = $data[1];
+       
+
+
+       
+    }
+  
+    fclose($open);
+  }
+
+
+  if($email==$email_saved ){
+
+    $info = array ($username, $email_saved, $newpassword);
+   
+    $fp = fopen('../storage/users.csv', 'w');
+    
+        if(fputcsv($fp, $info)){
+
+            echo  "Password Updated";
+        }
+
+  }
+
+  else{
+      echo "User does not exist";
+  }
+
+
 }
-echo "HANDLE THIS PAGE";
+
 
 
